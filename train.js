3470e7e6c604aac64c2d19a5998da8d53493e7a5
BotirjonUraimov <botirjon.uraimov.93@gmail.com>
2023-02-02 20:19:45 +0900

    BRR fix: modify image uploder utils with L-task

--- train.js
+++ train.js
@@ -1,5 +1,12 @@
 //  K-task
-const footballPoints = (wins, draws, losses) => {
-  return wins * 3 + draws * 1 + losses * 0;
-};
-console.log(footballPoints(3, 4, 1));
+// const footballPoints = (wins, draws, losses) => {
+//   return wins * 3 + draws * 1 + losses * 0;
+// };
+// console.log(footballPoints(3, 4, 1));
+
+//  L-Task
+function calculate(str) {
+  return eval(str);
+}
+
+console.log(calculate("5 * 5"));
