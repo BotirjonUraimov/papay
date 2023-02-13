b84a49a99e8081205221371021c65c1c081c15c9
BotirjonUraimov <botirjon.uraimov.93@gmail.com>
2023-02-13 20:07:14 +0900

    BRR fix: develop retaurant releted product control page on admin page

--- controllers/productController.js
+++ controllers/productController.js
@@ -27,8 +27,8 @@ productController.addNewProduct = async (req, res) => {
 
     const result = await product.addNewProductData(data, req.member);
 
-    const html = `<script>alert(new product added successfully);
-    window.location.replace(resto/products/menu); 
+    const html = `<script>alert("new product added successfully");
+    window.location.replace('/resto/products/menu'); 
     </script>`;
 
     res.end(html);
