28cd7a6dc9a39c39ec7c894c2c13a59bc5827f81
BotirjonUraimov <botirjon.uraimov.93@gmail.com>
2023-03-09 20:10:02 +0900

    BRR feat: develop getAllproduct restAPI bisiness logic

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const memberController = require("./controllers/memberController");
+const productController = require("./controllers/productController");
 
 /******************************
  *          REST API          *
@@ -11,23 +12,17 @@ router.post("/signup", memberController.signup);
 router.post("/login", memberController.login);
 router.get("/logout", memberController.logout);
 router.get("/check-me", memberController.checkMyAuthentication);
-router.post(
+router.get(
   "/member/:id",
   memberController.retriveAuthMember,
   memberController.getChosenMember
 );
 
-// other routers
-router.get("/menu", function (req, res) {
-  res.send(
-    "<h1 style = 'text-align:center; margin: 100px;'> You are in  <span style = 'color: green; font-weight: bold;'> MENU </span>  page </h1>"
-  );
-});
-
-router.get("/community", function (req, res) {
-  res.send(
-    "<h1 style = 'text-align:center; margin: 100px;'> You are in  <span style = 'color: green; font-weight: bold;'> COMMUNITY </span>  page </h1>"
-  );
-});
+// Product related routers
+router.post(
+  "/products",
+  memberController.retriveAuthMember,
+  productController.getAllProducts
+);
 
 module.exports = router;
